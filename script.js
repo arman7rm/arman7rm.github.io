59d147b49ae56b3f9815a7b21102b0c292ad018f
arman7rm <arman97rm@gmail.com>
2025-04-07 12:53:35 -0400

    adding football quiz

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
       });
     });
   });
+
+
+const multiply = (a,b) => {
+    return a*b;
+}
+
+const yell = (str) => {
+    console.log(String(str).toUpperCase());
+}
+
+function longerThan(a,b){
+    return a.length > b.length;
+}
